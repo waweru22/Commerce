99291acdac49ecc29ed618ba15eb8d890019a65d
Bhlak <bhlakcodes@proton.me>
2024-07-30 13:41:56 +0100

    Cleared local cache and added basic admin dashboard functionality

--- Signup/admin.py
+++ Signup/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import AppUser
 # from Signup.models import Vendor, Customer
 
 # Register your models here.
 
 # admin.site.register(Customer)
 # admin.site.register(Vendor)
+
+@admin.register(AppUser)
+class AppUserAdmin(UserAdmin):
+    model = AppUser
+    list_display = ["email", "username", "is_staff", "is_active"]
+
+    search_fields = ("email",)
+    ordering = ("email",)
