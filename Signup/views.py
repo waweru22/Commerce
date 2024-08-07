589efb431bc194826c54aa75f1342aa1fbfe5a08
Bhlak <bhlakcodes@proton.me>
2024-08-07 14:14:48 +0100

    merging

--- Signup/views.py
+++ Signup/views.py
@@ -29,6 +29,7 @@ class Users(generics.ListCreateAPIView):
     serializer_class = AppUserSerializer
     queryset = AppUser.objects.all()
     def post(self, request, *args, **kwargs):
+
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid(raise_exception = True):
